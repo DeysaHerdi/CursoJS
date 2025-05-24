e5cfb50f61bb302f84c5645853714325b1e1467c
Deysa <deysaherdy@hotmail.com>
2025-05-24 00:27:28 -0300

    10

---
+++ aula10/index.js
@@ -0,0 +1,10 @@
+const nome = 'Luiz'; // string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+
+const num1 = 10; // number
+const num2 = 10.10; // number
+const nomeAluno; // undefined
+const sobrenomeAluno = null;
+const aprovado = true; // boolean
+console.log(typeof nome, typeof num1, typeof num2, typeof nomeAluno, typeof sobrenomeAluno)
