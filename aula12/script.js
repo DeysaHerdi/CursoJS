f3b1a52bc2dd221d3df618d02de2be713e88a61e
Deysa <deysaherdy@hotmail.com>
2025-05-31 02:52:10 -0300

    aula12

---
+++ aula12/script.js
@@ -0,0 +1,9 @@
+alert("Oii")
+alert(confirm("Confirma?"))
+
+let nome = prompt("Qual seu nome?")
+alert(`Ola ${nome}`)
+
+const num1 = Number(prompt("Digite um numero"))
+const num2 = Number(prompt("Digite outro numero"))
+alert(num1 + num2)
