b9116e9d279d76dab8ffae39b35baefff1c10395
Deysa <deysaherdy@hotmail.com>
2025-06-04 01:14:13 -0300

    exerc13

--- aula13/index.js
+++ aula13/index.js
@@ -1,3 +1,11 @@
 let umaString = "Um \"texto\"";
 console.log(umaString)
 console.log(umaString[0])
+
+console.log(umaString.concat(" concatenado"))
+console.log(umaString + " concatenado")
+console.log(`${umaString} concatenado`)
+
+console.log(umaString.indexOf("x"))
+console.log(umaString.indexOf("o"))
+console.log(umaString.indexOf("o", 3))
