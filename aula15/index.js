2b1866ea30f31bd2651da17c46c60aae8a28a19a
Deysa <deysaherdy@hotmail.com>
2025-06-05 00:07:07 -0300

    aula15

---
+++ aula15/index.js
@@ -0,0 +1,13 @@
+let num1 = 9.49;
+let num2 = 9
+
+console.log(num2 ** 0.5); //raiz quadrada
+
+console.log(Math.ceil(num1));
+console.log(Math.floor(num1));
+console.log(Math.round(num1));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio);
