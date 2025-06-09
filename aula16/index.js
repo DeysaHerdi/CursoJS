ece023a253009f6642978772c647f838058ac2de
Deysa <deysaherdy@hotmail.com>
2025-06-09 15:02:40 -0300

    aula16

---
+++ aula16/index.js
@@ -0,0 +1,26 @@
+const alunos = ['Luiz', 'Maria', 'Joaquim']
+
+console.log(alunos)
+
+// alunos.push('Roberto')
+// alunos.push('Otavio')
+
+// console.log(alunos.slice(0, 3))
+
+/* // adiciona no final
+alunos[3] = 'Ana'
+alunos[alunos.length] = 'Pedro'
+alunos[alunos.length] = 'Rafael'
+
+alunos.push('Fabio') // adiciona no final
+alunos.unshift('Sofia') // adiciona no inicio
+
+const removido = alunos.pop() // remove o ultimo
+alunos.shift() // remove o primeiro
+
+delete alunos[1]
+
+console.log(removido)
+console.log(alunos)
+console.log(alunos[0], alunos[1], alunos[2])
+ */
