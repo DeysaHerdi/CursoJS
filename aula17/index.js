5bb2ad3dc2c7fc8083ab0f81bdad3d251f16430b
Deysa <deysaherdy@hotmail.com>
2025-06-09 22:54:44 -0300

    aula17

---
+++ aula17/index.js
@@ -0,0 +1,17 @@
+// function soma(x,y){
+//     const resultado = x + y
+//     return resultado
+// }
+
+// const resultado = soma(1,2)
+// console.log(resultado)
+
+// const raiz = function (n){
+//     return n ** 0.5
+// }
+
+// console.log(raiz(9))
+
+
+const raiz = n => n ** 0.5
+console.log(raiz(9))
