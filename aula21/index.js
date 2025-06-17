47c321d7bef007cbbadde4f52d3ce37465f61a2d
Deysa <deysaherdy@hotmail.com>
2025-06-17 02:44:49 -0300

    aula21

---
+++ aula21/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores de atribuição
+> maior que
+>= maior ou igual
+< menor que
+<= menor ou igual
+== igual (valor)
+=== identico (valor e tipo)
+!= diferente (valor)
+!== não identico (valor e tipo)
+*/
