9813dd7815d78933dee5aa2df0b06a1e06a867d1
Deysa <deysaherdy@hotmail.com>
2025-06-17 02:21:09 -0300

    aula20

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -17,7 +17,14 @@ function meuEscopo() {
     const peso = form.querySelector(".peso");
     const altura = form.querySelector(".altura");
 
-    
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value,
+    });
+
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
 
     console.log(pessoas)
   }
